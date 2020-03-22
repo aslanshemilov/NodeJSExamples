6ab0c061cc6304a0e0fb7d12f685d01e98b6d7f3
Shilpa Kancharla <shilpakancharla18@gmail.com>
2020-03-22 10:13:51 -0400

    Passport

--- conFusionServer/app.js
+++ conFusionServer/app.js
@@ -10,7 +10,12 @@ var usersRouter = require('./routes/users');
 var session = require('express-session');
 var FileStore = require('session-file-store')(session);
 
+var passport = require('passport');
+var authenticate = require('./authenticate');
+
 var app = express();
+app.use(passport.initialize());
+app.use(passport.session());
 
 var dishRouter = require('/routes/dishRouter');
 var promoRouter = require('./routes/promoRouter');
@@ -56,22 +61,15 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 function auth (req, res, next) {
-    console.log(req.session);
+  console.log(req.user);
 
-  if(!req.session.user) {
-      var err = new Error('You are not authenticated!');
-      err.status = 403;
-      return next(err);
+  if (!req.user) {
+    var err = new Error('You are not authenticated!');
+    err.status = 403;
+    next(err);
   }
   else {
-    if (req.session.user === 'authenticated') {
-      next();
-    }
-    else {
-      var err = new Error('You are not authenticated!');
-      err.status = 403;
-      return next(err);
-    }
+        next();
   }
 }
 
