beea6edf855552086a1ccca0f38d40f1fe0ad4b4
Shilpa Kancharla <shilpakancharla18@gmail.com>
2020-03-24 17:59:12 -0400

    Assignment 4

--- conFusionServer/app.js
+++ conFusionServer/app.js
@@ -1,46 +1,42 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var passport = require('passport')
+var config = require('./config')
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var userRouter = require('./routes/userRouter');
+var dishRouter = require('./routes/dishRouter')
+var promoRouter = require('./routes/promoRouter')
+var leaderRouter = require('./routes/leaderRouter')
+var uploadRouter = require('./routes/uploadRouter')
+var favoriteRouter = require('./routes/favoriteRouter')
+var commentRouter = require('./routes/commentRouter')
 
-var session = require('express-session');
-var FileStore = require('session-file-store')(session);
+const mongoose = require('mongoose')
 
-var passport = require('passport');
-var authenticate = require('./authenticate');
-
-var config = require('./config');
-
-const url = config.mongoUrl;
-
-var app = express();
-app.use(passport.initialize());
-app.use(passport.session());
-
-var dishRouter = require('/routes/dishRouter');
-var promoRouter = require('./routes/promoRouter');
-var leaderRouter = require('./routes/leaderRouter');
-
-const mongoose = require('mongoose');
-const Dishes = require('./models/dishes');
-const url = 'mongodb://localhost:27107/conFusion';
-const connect = mongoose.connect(url);
-
-const uploadRouter = require('./routes/uploadRouter');
+// Connect to MongoDB
+const url = config.mongoUrl
+const connect = mongoose.connect(url, { useNewUrlParser: true, useFindAndModify: false, useCreateIndex: true, useUnifiedTopology: true })
 
 connect.then((db) => {
-  console.log("Connected correctly to server");
-}, (err) => { console.log(err); });
+  console.log('Connected correctly to server!')
+}), (err) => { console.log(err) }
 
-app.use('/imageUpload', uploadRouter);
 
-app.use('/dishes', dishRouter);
-app.use('/promotions', promoRouter);
-app.use('/leaders', leaderRouter);
+
+var app = express();
+
+// redirect traffic to secure port
+app.all('*', (req, res, next) => {
+  if (req.secure) {
+    return next()
+  }
+  else {
+    res.redirect(307, 'https://' + req.hostname + ':' + app.get('secPort') + req.url)
+  }
+})
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -49,37 +45,20 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+
+app.use(passport.initialize())
+
+app.use('/', indexRouter);
+app.use('/users', userRouter);
+
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-app.use(cookieParser('12345-67890-09876-54321'));
-
-app.use(session({
-  name: 'session-id',
-  secret: '12345-67890-09876-54321',
-  saveUninitialized: false,
-  resave: false,
-  store: new FileStore()
-}));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-function auth (req, res, next) {
-  console.log(req.user);
-
-  if (!req.user) {
-    var err = new Error('You are not authenticated!');
-    err.status = 403;
-    next(err);
-  }
-  else {
-        next();
-  }
-}
+app.use('/dishes', dishRouter);
+app.use('/promotions', promoRouter);
+app.use('/leaders', leaderRouter);
+app.use('/imageUpload', uploadRouter);
+app.use('/favorites', favoriteRouter)
+app.use('/comments', commentRouter)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -97,16 +76,4 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-app.use(auth);
-
 module.exports = app;
-
-// Secure traffic only
-app.all('*', (req, res, next) => {
-  if (req.secure) {
-    return next();
-  }
-  else {
-    res.redirect(307, 'https://' + req.hostname + ':' + app.get('secPort') + req.url);
-  }
-});
